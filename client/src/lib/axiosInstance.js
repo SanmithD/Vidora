de393b0e3d4576c95b3236078ea5ced0acde84c0
SanmithD <sanmithdevadiga7@gmail.com>
2025-06-24 11:15:15 +0530

    url added

--- client/src/lib/axiosInstance.js
+++ client/src/lib/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const AxiosInstance = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://vidora-xygf.onrender.com/api',
     withCredentials: true
 });
